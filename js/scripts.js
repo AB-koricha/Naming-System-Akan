fe48626dc326ebe8c256913d8ed844b951213bad
abdirahman <27870abdirahmanabdidict19s@gmail.com>
2020-05-20 10:33:32 +0300

    add Scripts.js to the page to check on whether date and month enter is valid or invalid and if invalid it alert

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var date=31;
+if(date > 0){
+  console.log("valid");
+}else if(date < 30){
+  console.log("valid");
+}else{
+  alert("invalid date");
+}
+var month=12;
+if(month > 0){
+  console.log("valid");
+}else if(month < 12){
+  console.log("valid");
+}else {
+  alert("invalid month")
+}
