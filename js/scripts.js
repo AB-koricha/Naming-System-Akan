9fb9454ccbf50923c6115c546193a790be53c073
abdirahman <27870abdirahmanabdidict19s@gmail.com>
2020-05-20 11:29:01 +0300

    add scripts.js to the page to calculate the day of the week they were born

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ if(date > 0){
 }else if(date < 30){
   console.log("valid");
 }else{
-  alert("invalid date");
+  alert("enter valid one");
 }
 var month=12;
 if(month > 0){
@@ -12,5 +12,16 @@ if(month > 0){
 }else if(month < 12){
   console.log("valid");
 }else {
-  alert("invalid month")
+  alert("enter valid one")
 }
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
